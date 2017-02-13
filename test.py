62145292b9b26f4f97b928dff2242d67ad16c0d2
Maksim <servelat20kg@gmail.com>
2017-02-13 12:33:26 +0200

    Testing file

---
+++ test.py
@@ -0,0 +1,16 @@
+import dis
+
+def foo():
+    pass
+    x = 3
+    return True
+
+text1 = '"- I like Python! Do you?\n- Yes, very much."'
+text2 = "\"I don't want to go home.\""
+text3 = '"He said: "No, I don\'t know""'
+text4 = "\"You canuse # to comment a line.\""
+print text1
+print text2
+print text3
+print text4
+print text1[::-1]
